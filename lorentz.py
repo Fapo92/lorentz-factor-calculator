be3582b0ace843424d0801d0b66a32dd69906172
Fabrizio Pomata <fabriziopomata@pop-os.localdomain>
2020-04-25 00:43:26 -0400

    .

---
+++ lorentz.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+#caluclar factor gamma para distintas velocidades
+
+import cmath
+from math import sqrt
+from decimal import Decimal
+
+v = 0.0
+c = 1
+resultado = 0.0
+welcome = """
+    **********************************************
+    *       Welcome to the Lorentz factor        *
+    *               calculator.                  *  
+    *                                            *    
+    * Use: input a velocity in c (example: 0.5c) *
+    * to obtain its factor. Repeat as many times *
+    * as you need to.                            *
+    *                                            *
+    * To exit just stop the terminal process or  *
+    * write 'exit' + enter.                      *                       
+    **********************************************
+    """
+
+def gamma():
+    v = Decimal(input("Input the relative velocity v: "))
+    if  v < 1:
+        resultado = 1/(sqrt(1-(v/c)**2))
+        print(resultado)
+    elif v > 1:
+        resultado = 1/(cmath.sqrt(1-(v/c)**2))
+        print(resultado)
+    elif v == 1:
+        print("The Lorentz factor goes to infinity when v = c. Try something else.")
+
+def main():
+    print(welcome)
+    key = input("Press enter to continue...") 
+    while key != 'exit':
+        gamma()
+        key = input("Press enter to continue...")
+
+if __name__ == "__main__":
+    main()
